868e63ecf113cbebbbee477fe10077291f52681e
duskohu <duskohu@gmail.com>
2013-07-13 01:18:13 +0200

    Updates for newest NasExt/Forms

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -28,7 +28,8 @@ class HomepagePresenter extends BasePresenter
 
 		$range = new Range(10, 100);
 		$form->addRangeslider('rangeSlider', 'Set range', $range)
-			->setDefaultValue(new Range(15, 33))
+			->setAttribute('data-custom-init', Nette\Utils\Json::encode(array('step'=>2)))
+			->setDefaultValue(new Range(15, 66))
 			->addRule($form::FILLED, 'Please complete mandatory field')
 			->addRule($form::INTEGER, 'Please enter a numeric value')
 			->addRule($form::RANGE, 'Please enter a value between %d and %d', $range->getRange());
